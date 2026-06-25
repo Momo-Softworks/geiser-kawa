e4eccb51ef8a5fdf1724d99ef27fb7bf909e8475
Samuel Willey <swilley@momo-softworks.com>
2026-06-25 18:15:43 -0500

    Revert unnecessary buffer association code (C-c C-z handles it)

--- elisp/geiser-kawa-connect.el
+++ elisp/geiser-kawa-connect.el
@@ -8,8 +8,7 @@
 ;;; Commentary:
 ;;
 ;; Connect Geiser to an already-running Kawa TCP REPL (port 4243 standalone,
-;; 4242 in-game).  The command also associates the current buffer with the
-;; REPL so that completions and eval work immediately.
+;; 4242 in-game).  After connecting, use C-c C-z to associate the buffer.
 
 ;;; Code:
 
@@ -18,18 +17,13 @@
 ;;;###autoload
 (defun geiser-kawa-connect (&optional host port)
   "Connect to a running Kawa REPL on HOST and PORT.
-Associates the current buffer with the REPL for completions and eval."
+HOST defaults to \"localhost\".  Use C-c C-z after connecting to
+associate the current buffer with the REPL."
   (interactive
    (list (read-string "Kawa host (default localhost): " nil nil "localhost")
          (read-number "Kawa port: ")))
   (require 'geiser-kawa)
-  (geiser-connect 'kawa (or host "localhost") port)
-  ;; Associate the current buffer with the new REPL.
-  (when (derived-mode-p 'scheme-mode)
-    (setq geiser-impl--implementation 'kawa)
-    (require 'geiser-repl)
-    (geiser-repl--switch-to-buffer 'kawa)
-    (geiser-repl--switch-to-buffer 'kawa)))
+  (geiser-connect 'kawa (or host "localhost") port))
 
 (provide 'geiser-kawa-connect)
 
